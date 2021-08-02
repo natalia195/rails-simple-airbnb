6dd2b783ffe65523134a426c7ff5c2b7cd30c9cd
natalia covarrubias <natalia.covarrubias@ug.uchile.cl>
2021-08-02 13:03:30 -0400

    Avance primer día

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Stylish House Close to River Thames',
+  address: Faker::Address.street_address,
+  description: Faker::Lorem.paragraph,
+  price_per_night: 80,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: Faker::Address.street_address,
+  description: Faker::Lorem.paragraph,
+  price_per_night: 100,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'Beutiful flat Santiago',
+  address: Faker::Address.street_address,
+  description: Faker::Lorem.paragraph,
+  price_per_night: 60,
+  number_of_guests: 3
+)
